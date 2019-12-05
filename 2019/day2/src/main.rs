d50e3ea77a49d7c408d50949763c61bd53b71cf2
Koen Bolhuis <koen.bolhuis@gmail.com>
2019-12-05 18:37:06 +0100

    Use Intcode crate for day 2

--- 2019/day2/src/main.rs
+++ 2019/day2/src/main.rs
@@ -1,8 +1,9 @@
 use std::fs;
-use std::convert::TryInto;
+
+use intcode;
 
 fn main() {
-    let input: Vec<u32> = fs::read_to_string("input")
+    let input: Vec<i64> = fs::read_to_string("input")
         .unwrap()
         .split(',')
         .map(|n| n.trim().parse().unwrap())
@@ -12,39 +13,16 @@ fn main() {
     part2(input.clone());
 }
 
-fn interpret(mem: &mut [u32]) {
-    let mut pc = 0;
-    loop {
-        let opcode = mem[pc];
-        match opcode {
-            1 | 2 => {
-                let a: usize = mem[pc + 1].try_into().unwrap();
-                let b: usize = mem[pc + 2].try_into().unwrap();
-                let dest: usize = mem[pc + 3].try_into().unwrap();
-
-                if opcode == 1 {
-                    mem[dest] = mem[a] + mem[b];
-                } else if opcode == 2 {
-                    mem[dest] = mem[a] * mem[b];
-                }
-            }
-            99 => break,
-            _ => panic!("Unknown opcode {}", mem[pc]),
-        }
-        pc += 4;
-    }
-}
-
-fn part1(mut input: Vec<u32>) {
+fn part1(mut input: Vec<i64>) {
     input[1] = 12;
     input[2] = 2;
 
-    interpret(&mut input);
+    intcode::interpret(&mut input);
 
     println!("{}", input[0]);
 }
 
-fn part2(input: Vec<u32>) {
+fn part2(input: Vec<i64>) {
     let mut mem = vec![0; input.len()];
 
     let mut done = false;
@@ -54,7 +32,7 @@ fn part2(input: Vec<u32>) {
             mem[1] = noun;
             mem[2] = verb;
 
-            interpret(&mut mem);
+            intcode::interpret(&mut mem);
 
             if mem[0] == 19690720 {
                 println!("{}", 100 * noun + verb);
